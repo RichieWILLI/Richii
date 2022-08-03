9c5f9b3691cb7c390975af3786d5265d2f799700
RichieWilli <richardwilliam1994@gmail.com>
2022-08-04 00:17:30 +0530

    Pushing the code to github by engineer 1

---
+++ src/test/java/org/dummy/gitbash/SampleScript.java
@@ -0,0 +1,13 @@
+package org.dummy.gitbash;
+
+public class SampleScript {
+
+	public static void main(String[] args) {
+
+		System.out.println("hello how r u?");
+		System.out.println("raju");
+		
+		
+	}
+
+}
